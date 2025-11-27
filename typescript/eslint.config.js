2f6c77fec1a53c84c44809cce40cd42dadea5e5e
jrnesc <78368044+jrnesc@users.noreply.github.com>
2025-11-27 18:19:22 +0000

    init to win it
    - adding Typescript sdk check + bulkCheck

---
+++ typescript/eslint.config.js
@@ -0,0 +1,38 @@
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const tseslintParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        project: ["./tsconfig.json", "./examples/tsconfig.json"],
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-return-await": "error",
+      "prefer-const": "error",
+      eqeqeq: ["error", "always", { null: "ignore" }],
+      semi: ["error", "always"],
+      quotes: ["error", "double", { avoidEscape: true }],
+      indent: ["error", 2],
+      "comma-dangle": ["error", "always-multiline"],
+    },
+  },
+];
